88981d377a397d80d6a694f4e1f6c2d75fd46782
Пташник Роман Вікторович <128740344+ptashnykroman@users.noreply.github.com>
2026-02-06 14:56:07 +0200

    Modify CORS settings to use trusted origins
    Updated CORS configuration to use trusted origins from environment variable.

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,8 @@ async function bootstrap() {
 
   app.useStaticAssets(join(process.cwd(), 'upload'), { prefix: '/upload/' });
 
-  // app.enableCors({ origin: 'http://localhost:5173', credentials: true });
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({ origin: [...(process.env.TRUSTED_ORIGINS || '').split(',')], credentials: true });
+  // app.enableCors({ origin: true, credentials: true });
 
   const config = new DocumentBuilder()
     .setTitle('Pharm couses API')
@@ -40,3 +40,4 @@ async function bootstrap() {
   }
 }
 bootstrap();
+
