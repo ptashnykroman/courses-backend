0356e04af6b002487cd102a3ccce68261bacf9a6
Пташник Роман Вікторович <128740344+ptashnykroman@users.noreply.github.com>
2026-02-09 15:50:36 +0200

    Implement email verification in auth module
    Added email verification functionality using Resend service.

--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
+import { Resend } from 'resend';
 
 dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
 
@@ -9,6 +10,8 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { admin, multiSession } from 'better-auth/plugins';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../../../prisma/generated/client';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -25,6 +28,33 @@ export const auth = betterAuth({
   trustedOrigins: [...(process.env.TRUSTED_ORIGINS || '').split(',')],
   emailAndPassword: { enabled: true },
   plugins: [admin(), multiSession()],
+
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        // from: 'Курси БПР <noreply@pharm.zt.ua>',
+        to: user.email,
+        subject: 'Підтвердження електронної пошти — курси БПР ЖБФФК',
+        html: `
+        <h1>Вітаємо!</h1>
+
+        <p>Ви отримали цей лист, оскільки реєструвалися на платформі курсів безперервного професійного розвитку (БПР)
+        Житомирського базового фармацевтичного фахового коледжу.</p>
+
+        <p>Щоб завершити реєстрацію та підтвердити свою електронну пошту, будь ласка, перейдіть за посиланням нижче:</p>
+
+        <p><a href="${url}">[Підтвердити електронну пошту]</a></p>
+
+        <p>Якщо ви не реєструвалися на нашому сайті, просто проігноруйте цей лист.</p>
+
+        <p>З повагою</p>
+        <p>Команда курсів БПР</p>
+        <p>Житомирський базовий фармацевтичний фаховий коледж</p>
+        `,
+      });
+    },
+  },
   
   user: {
     additionalFields: {
@@ -62,6 +92,7 @@ export const auth = betterAuth({
     }
   },
 });
+
 
 
 
